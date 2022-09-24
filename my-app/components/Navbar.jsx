81ee3146438439b0e93303163738a7f1d898cbf3
Zachary Gaines <zacharygaines28@gmail.com>
2022-09-23 23:04:45 -0700

    mobile menu logo and outline close

--- my-app/components/Navbar.jsx
+++ my-app/components/Navbar.jsx
@@ -1,47 +1,62 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+/* eslint-disable jsx-a11y/alt-text */
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 // import img from '../public/assets/ZGNav.png'
- 
 
-function Navbar() {
-    return (
-      <div className='fixed w-full h-20 shadow-xl z-[100]'>
-        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-          <Image src="/../public/assets/ZGNav.png" alt="/" width='75' height='70' 
-          />
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <Image src="/../public/assets/ZGNav.png" alt="/" width='75' height='70'
+        />
+        <div>
+          <ul className='hidden md:flex'>
+            <Link href='/'>
+              <li className="ml-10 text-sm uppercase hover:border-b">HOME</li>
+            </Link>
+            <Link href='/'>
+              <li className="ml-10 text-sm uppercase hover:border-b">ABOUT</li>
+            </Link>
+            <Link href='/'>
+              <li className="ml-10 text-sm uppercase hover:border-b">SKILLS</li>
+            </Link>
+            <Link href='/'>
+              <li className="ml-10 text-sm uppercase hover:border-b">PROJECTS</li>
+            </Link>
+            <Link href='/'>
+              <li className="ml-10 text-sm uppercase hover:border-b">CONTACT</li>
+            </Link>
+          </ul>
+          <div className='md:hidden'>
+
+
+            <AiOutlineMenu size={25} />
+
+
+
+          </div>
+        </div>
+
+      </div>
+      <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+        <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
           <div>
-            <ul className='hidden md:flex'>
-              <Link href='/'>
-                <li className="ml-10 text-sm uppercase hover:border-b">HOME</li>
-              </Link>
-              <Link href='/'>
-                <li className="ml-10 text-sm uppercase hover:border-b">ABOUT</li>
-              </Link>
-              <Link href='/'>
-                <li className="ml-10 text-sm uppercase hover:border-b">SKILLS</li>
-              </Link>
-              <Link href='/'>
-                <li className="ml-10 text-sm uppercase hover:border-b">PROJECTS</li>
-              </Link>
-              <Link href='/'>
-                <li className="ml-10 text-sm uppercase hover:border-b">CONTACT</li>
-              </Link>
-            </ul>
-            <div>
+            <div className='flex w-full items-center justify-between'>
 
-
-              {/* https://react-icons.github.io/react-icons/ */}
-
-
-              
+              <Image src='/../public/assets/ZGNav.png' width='50' height='45' alt='/' />
+              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                <AiOutlineClose />
+              </div>
             </div>
           </div>
-
         </div>
-        
       </div>
-    )
-  }
+
+    </div>
+  )
+}
 
 export default Navbar
