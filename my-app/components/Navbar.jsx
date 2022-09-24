7ee0b331e4ff733e56209d163b0e54a922240843
Zachary Gaines <zacharygaines28@gmail.com>
2022-09-23 23:30:11 -0700

    mobile app social media logos

--- my-app/components/Navbar.jsx
+++ my-app/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
 // import img from '../public/assets/ZGNav.png'
 
 
@@ -49,6 +50,34 @@ const Navbar = () => {
               <Image src='/../public/assets/ZGNav.png' width='50' height='45' alt='/' />
               <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                 <AiOutlineClose />
+              </div>
+            </div>
+            <div className='border-b border-gray-300 my-4'>
+              <p className='w-[85%] md:w-[90%] py-4'>WOOP WOOP</p>
+            </div>
+          </div>
+          <div className='py-4 flex flex-col'>
+            <ul className='uppercase'>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Home</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>About</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Skills</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Projects</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Contact</li>
+              </Link>
+            </ul>
+            <div className='pt-40'>
+              <p className='uppercase tracking-widest text-[#5651e5]'>Let's Connect</p>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                <FaLinkedinIn />
               </div>
             </div>
           </div>
