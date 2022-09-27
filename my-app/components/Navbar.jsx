075cf83c1b1b71a8791d0529f181cefde71a5baf
Zachary Gaines <zacharygaines28@gmail.com>
2022-09-26 21:27:55 -0700

    find the error

--- my-app/components/Navbar.jsx
+++ my-app/components/Navbar.jsx
@@ -1,14 +1,20 @@
 /* eslint-disable jsx-a11y/alt-text */
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from 'react-icons/ai';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-// import img from '../public/assets/ZGNav.png'
+
 
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
@@ -32,7 +38,7 @@ const Navbar = () => {
               <li className="ml-10 text-sm uppercase hover:border-b">CONTACT</li>
             </Link>
           </ul>
-          <div className='md:hidden'>
+          <div onClick={handleNav} className='md:hidden'>
 
 
             <AiOutlineMenu size={25} />
@@ -43,13 +49,13 @@ const Navbar = () => {
         </div>
 
       </div>
-      <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
-        <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+      <div className={nav ? 'fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+        <div className={nav ? 'fixed left-[-10%] top-0 ease-in duration-500' : ''}>
           <div>
             <div className='flex w-full items-center justify-between'>
 
               <Image src='/../public/assets/ZGNav.png' width='50' height='45' alt='/' />
-              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+              <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                 <AiOutlineClose />
               </div>
             </div>
@@ -95,7 +101,7 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
