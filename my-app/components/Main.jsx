bbaa8327d2dbe4f9ae606c107fc1ca1fcd951eb5
Zachary Gaines <zacharygaines28@gmail.com>
2022-11-04 20:36:21 -0700

    a little css

--- my-app/components/Main.jsx
+++ my-app/components/Main.jsx
@@ -9,9 +9,9 @@ const Main = () => {
       <div className='max-w-{1240px} w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
           {/* <p className='uppercase text-sm tracking-widest text-gray-600'></p> */}
-          <h1 className='text-[#00308F] py-4 tracking-widest max-auto'>Hi, Im Zach</h1>
-          <h1 className='text-[#6699CC]'>a front end web developer.</h1>
-          <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I'm a front end developer specializing in blowing your mind. </p>
+          <h1 className='text-[#0e182e]'>Hi, I&apos;m <span className='text-[#0082b6bb]'>Zach</span> </h1>
+          <h1 className='text-[#0e182e]'>A Full-Stack Web Developer.</h1>
+          <p className='py-4 text-gray-666 max-w-[70%] m-auto'>I&apos;m a front end developer specializing in blowing your mind. </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
               <FaLinkedinIn />
