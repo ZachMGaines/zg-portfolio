55d4b915b13ddd2c5b3c7d95884733db53db58c7
Zachary Gaines <zacharygaines28@gmail.com>
2022-11-04 21:03:59 -0700

    about page

---
+++ my-app/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full md:h-screen p-2 flex items-center py-16'>
+      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+        <div className='col-span-2'>
+          <p className='uppercase text-xl tracking-widest text-[#0082b6bb]'>About</p>
+          <h2 className='py-4'>Who I am</h2>
+          <p className='py-2 text-gray-600'>// I am not your average developer</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
