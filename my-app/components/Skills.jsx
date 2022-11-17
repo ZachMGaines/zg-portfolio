c49456cfb7cdd561539447082bc3037069463cc1
Zachary Gaines <zacharygaines28@gmail.com>
2022-11-16 21:19:50 -0800

    skills border boxes

--- my-app/components/Skills.jsx
+++ my-app/components/Skills.jsx
@@ -8,43 +8,43 @@ const Skills = () => {
         <div className='col-span-2'>
           <p className='uppercase text-xl tracking-widest text-[#0082b6bb]'>Skills</p>
           <h2 className='py-4'>What I Can Do</h2>
+          <div>
 
-          <div className='lg:grid grid-cols-3 gap-8'>
+            <div className='lg:grid grid-cols-3 gap-8 justify-between'>
+              <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                <h4 className='text-[#2a3d92] font-bold text-2xl py-px'>Languages</h4>
+                <ul className='list-disc font-bold'>
+                  <li>Javascript</li>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                </ul>
+              </div>
+              <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                <h4 className='text-[#2a3d92] font-bold text-2xl py-px'>Libraries</h4>
+                <ul className='list-disc font-bold'>
+                  <li>React</li>
+                  <li>Node.js</li>
+                  <li>Express.js</li>
+                  <li>Tailwind</li>
+                  <li>Next.js</li>
+                </ul>
+              </div>
+              <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                <h4 className='text-[#2a3d92] font-bold text-2xl py-px'>Tools</h4>
+                <ul className='list-disc font-bold'>
+                  <li>VS code</li>
+                  <li>Postman</li>
+                  <li>PGadmin</li>
+                  <li>Heroku</li>
+                  <li>Netlify</li>
 
-            <div>
-              <h4>Languages</h4>
-              <ul className='list-disc'>
-                <li>javascript</li>
-                <li>html</li>
-                <li>css</li>
-              </ul>
-            </div>
-            <div>
-              <h4>Libraries</h4>
-              <ul className='list-disc'>
-                <li>React</li>
-                <li>Node.js</li>
-                <li>Express.js</li>
-                <li>Tailwind</li>
-                <li>Next.js</li>
-              </ul>
-            </div>
-            <div>
-              <h4>Tools</h4>
-              <ul className='list-disc'>
-                <li>VS code</li>
-                <li>Postman</li>
-                <li>PGadmin</li>
-                <li>Heroku</li>
-                <li>Netlify</li>
-
-              </ul>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-
   )
 }
 
